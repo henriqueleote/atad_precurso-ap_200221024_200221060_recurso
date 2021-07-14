62c5c083b50752bd8e7dadbba7cac3e8c1edc4a8
Henrique Leote <200221060@estudantes.ips.pt>
2021-07-14 16:52:27 +0100

    Comentários adicionados

--- netflix.c
+++ netflix.c
@@ -5,20 +5,20 @@
 
 Netflix netflixCreate(char *show_id, char *type, char *title, char *director, char *cast, char *country, char *date_added, char *release_year, char *rating, char *duration, char *listed_in, char *description){
     
-    Netflix netflix;
+    Netflix netflix;                                    //Creates a netflix struct object
 
-    strcpy(netflix.show_id, show_id);
-    strcpy(netflix.type, type);
-    strcpy(netflix.title, title);
-    strcpy(netflix.director, director);
-    strcpy(netflix.cast, cast);
-    strcpy(netflix.country, country);
-    strcpy(netflix.date_added, date_added);
-    strcpy(netflix.release_year, release_year);
-    strcpy(netflix.rating, rating);
-    strcpy(netflix.duration, duration);
-    strcpy(netflix.listed_in, listed_in);
-    strcpy(netflix.description, description);
+    strcpy(netflix.show_id, show_id);                   //Copy the string received to the netflix struct object
+    strcpy(netflix.type, type);                         //Copy the string received to the netflix struct object
+    strcpy(netflix.title, title);                       //Copy the string received to the netflix struct object
+    strcpy(netflix.director, director);                 //Copy the string received to the netflix struct object
+    strcpy(netflix.cast, cast);                         //Copy the string received to the netflix struct object
+    strcpy(netflix.country, country);                   //Copy the string received to the netflix struct object
+    strcpy(netflix.date_added, date_added);             //Copy the string received to the netflix struct object
+    strcpy(netflix.release_year, release_year);         //Copy the string received to the netflix struct object
+    strcpy(netflix.rating, rating);                     //Copy the string received to the netflix struct object
+    strcpy(netflix.duration, duration);                 //Copy the string received to the netflix struct object
+    strcpy(netflix.listed_in, listed_in);               //Copy the string received to the netflix struct object
+    strcpy(netflix.description, description);           //Copy the string received to the netflix struct object
 
-    return netflix;
+    return netflix; //Returns the netflix struct object
 }
